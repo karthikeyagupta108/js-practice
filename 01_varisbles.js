be43462165ff462b082c11efc0f2c80303c325c1
Karthikeya Gupta <karthikeyagupta11@gmail.com>
2025-10-10 12:04:56 +0530

    i am commitd

---
+++ 01_varisbles.js
@@ -0,0 +1,25 @@
+
+const account_Id = 144553
+let account_email = "karthikeya@gmail"
+var account_password = "123786"
+accountCity = "vizag"
+
+//account_Id = 234
+
+console.log(account_Id)
+
+
+// alert(3+3) we are using node js not browser
+
+let name = "karthik"
+let age = 18
+let loggeedin = true
+
+//null
+// undefined
+let dens = "" 
+// different from 
+let densi = null
+console.log(typeof age)
+console.log(typeof densi)
+console.log(typeof undefined)
