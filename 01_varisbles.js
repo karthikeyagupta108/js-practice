a98681efd61e9e37e6d0ad0b104a11b35d07cc2e
Karthikeya Gupta <karthikeyagupta11@gmail.com>
2025-10-11 21:41:29 +0530

    finalist

--- 01_varisbles.js
+++ 01_varisbles.js
@@ -1,126 +1,160 @@
 
-// const account_Id = 144553
-// let account_email = "karthikeya@gmail"
-// var account_password = "123786"
-// accountCity = "vizag"
+// // const account_Id = 144553
+// // let account_email = "karthikeya@gmail"
+// // var account_password = "123786"
+// // accountCity = "vizag"
 
-// //account_Id = 234
+// // //account_Id = 234
 
-// console.log(account_Id)
-
-
-// // alert(3+3) we are using node js not browser
-
-// let name = "karthik"
-// let age = 18
-// let loggeedin = true
-
-// //null
-// // undefined
-// let dens = "" 
-// // different from 
-// let densi = null
-// // console.log(typeof age)
-// // console.log(typeof densi)
-// // console.log(typeof undefined)
-
-// //let score= "33abc" // you get a NaN 
-// //let valueinnumber = Number(score)
-// //console.log(typeof valueinnumber)
-// //console.log(valueinnumber)
+// // console.log(account_Id)
 
 
-// // let nums = 908
-// // let stringnum = String(nums)
-// // console.log(typeof stringnum)
-// // console.log(stringnum)
+// // // alert(3+3) we are using node js not browser
+
+// // let name = "karthik"
+// // let age = 18
+// // let loggeedin = true
+
+// // //null
+// // // undefined
+// // let dens = "" 
+// // // different from 
+// // let densi = null
+// // // console.log(typeof age)
+// // // console.log(typeof densi)
+// // // console.log(typeof undefined)
+
+// // //let score= "33abc" // you get a NaN 
+// // //let valueinnumber = Number(score)
+// // //console.log(typeof valueinnumber)
+// // //console.log(valueinnumber)
 
 
+// // // let nums = 908
+// // // let stringnum = String(nums)
+// // // console.log(typeof stringnum)
+// // // console.log(stringnum)
 
 
 
-// // ********************OPERATIONS*********************
-
-// console.log(2 + 2)
-// // all addition, subtraction and all can be replaced in place of +
-
-// str1 = "hello"
-// str2 = " world"
-// let str3 = str1 + str2
-// console.log(str3)
-
-// console.log("1"+2)
-// console.log(1+"2")
-// console.log("1"+"2")
-
-// let newnum = 10
-// newnum++
-// console.log(newnum)
-
-// // important
-// console.log(1 + 2 + "1")
 
 
-// console.log(2 > 1)
-// // all regular operations are possible boolean value is returned 
+// // // ********************OPERATIONS*********************
 
-// // ===
-// //strict check
-// //it checks both data type and value
+// // console.log(2 + 2)
+// // // all addition, subtraction and all can be replaced in place of +
 
-// // Notes on perspective of interview
-// //primitive and non primitive
-// //based on call by value and call by reference
-// /*
-// 7 types
-// string ,number ,boolean ,null ,symbol,BigInt,undefined
+// // str1 = "hello"
+// // str2 = " world"
+// // let str3 = str1 + str2
+// // console.log(str3)
+
+// // console.log("1"+2)
+// // console.log(1+"2")
+// // console.log("1"+"2")
+
+// // let newnum = 10
+// // newnum++
+// // console.log(newnum)
+
+// // // important
+// // console.log(1 + 2 + "1")
 
 
-// reference (non primitive)
-// // return type of non primitive data types is object and if it is 
-// a function, it is object function
+// // console.log(2 > 1)
+// // // all regular operations are possible boolean value is returned 
 
-// arrays objects functions
-// */
-// const id = Symbol('123')
-// const newid = Symbol('123')
-// console.log(id === newid)
-//  console.log(typeof id);
+// // // ===
+// // //strict check
+// // //it checks both data type and value
+
+// // // Notes on perspective of interview
+// // //primitive and non primitive
+// // //based on call by value and call by reference
+// // /*
+// // 7 types
+// // string ,number ,boolean ,null ,symbol,BigInt,undefined
+
+
+// // reference (non primitive)
+// // // return type of non primitive data types is object and if it is 
+// // a function, it is object function
+
+// // arrays objects functions
+// // */
+// // const id = Symbol('123')
+// // const newid = Symbol('123')
+// // console.log(id === newid)
+// //  console.log(typeof id);
  
-// // array 
-// const heroes = [1, 2, 3, 4, "ert"]
+// // // array 
+// // const heroes = [1, 2, 3, 4, "ert"]
 
-// // object
+// // // object
 
-// let myobj = {
-//     name: "karthik",
-//     age:22
-// }
-// console.log(myobj.name);
+// // let myobj = {
+// //     name: "karthik",
+// //     age:22
+// // }
+// // console.log(myobj.name);
 
-// let myfun = function () {
-//     console.log("hello world")
-// }
-// myfun()
-// console.log(typeof myfun);
+// // let myfun = function () {
+// //     console.log("hello world")
+// // }
+// // myfun()
+// // console.log(typeof myfun);
 
-// // stack (primitive)
-// // heap (non primitive)
+// // // stack (primitive)
+// // // heap (non primitive)
 
-// let userone = {
-//     email: "yuser@gmail",
-//     age: "hjkfl"
-// }
+// // let userone = {
+// //     email: "yuser@gmail",
+// //     age: "hjkfl"
+// // }
 
-// usertwo = userone
-// usertwo.email = "yeasuser"
-// console.log(userone.email);
+// // usertwo = userone
+// // usertwo.email = "yeasuser"
+// // console.log(userone.email);
 
-// //when you keep in HTMLTableCaptionElement, you get 
-// referrence and see 
-// //above example 
+// // //when you keep in HTMLTableCaptionElement, you get 
+// // referrence and see 
+// // //above example 
 
 
-const name = "karthik"
-const repocount = 678
-console.log(`hello my name is ${name} and my repo count is ${repocount}`)
+// const name = "karthik"
+// const repocount = 678
+// console.log(`hello my name is ${name} and my repo count is ${repocount}`)
+
+
+// let s1 = "hello";
+// console.log(typeof s1); // "string"
+
+
+// s1.toUpperCase();
+// console.log(s1); // "hello" (unchanged)
+
+
+// let s2 = new String("hello");
+// console.log(typeof s2); // "object"
+
+
+// let s = "hello";
+// console.log(s.length); // works fine
+
+// //Even though "hello" is primitive, JS internally does this:
+// //new String("hello").length
+// //let a = "hello";
+// //let b = new String("hello");
+
+// // console.log(a == b);  // true  (values compared after type conversion)
+// // console.log(a === b); // false (different types: string vs object)
+
+// console.log(s.charAt(1))
+
+const myname = "karthikeya"
+
+const subname = myname.substring(0, 6)
+console.log(subname)
+
+// replace and includes functions with string
+
---
+++ numsandmaths.js
@@ -0,0 +1,20 @@
+const score = 100
+// console.log(score)
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// // score == balance   // true
+// // score === balance  // false
+// console.log(balance.toFixed(2))
+// const ell = 292.3456
+// console.log(ell.toPrecision(4))
+// // you have something to add commas 
+
+
+// **********************.  MATHS*****************
+
+console.log(Math.round(4.3423))
+
+
+
