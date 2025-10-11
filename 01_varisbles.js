c807753c482cdde7014a7baad99f45b43e1adb7d
Karthikeya Gupta <karthikeyagupta11@gmail.com>
2025-10-11 10:13:14 +0530

    trying refresh

--- 01_varisbles.js
+++ 01_varisbles.js
@@ -20,6 +20,87 @@ let loggeedin = true
 let dens = "" 
 // different from 
 let densi = null
-console.log(typeof age)
-console.log(typeof densi)
-console.log(typeof undefined)
+// console.log(typeof age)
+// console.log(typeof densi)
+// console.log(typeof undefined)
+
+//let score= "33abc" // you get a NaN 
+//let valueinnumber = Number(score)
+//console.log(typeof valueinnumber)
+//console.log(valueinnumber)
+
+
+// let nums = 908
+// let stringnum = String(nums)
+// console.log(typeof stringnum)
+// console.log(stringnum)
+
+
+
+
+
+// ********************OPERATIONS*********************
+
+console.log(2 + 2)
+// all addition, subtraction and all can be replaced in place of +
+
+str1 = "hello"
+str2 = " world"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+"2")
+
+let newnum = 10
+newnum++
+console.log(newnum)
+
+// important
+console.log(1 + 2 + "1")
+
+
+console.log(2 > 1)
+// all regular operations are possible boolean value is returned 
+
+// ===
+//strict check
+//it checks both data type and value
+
+// Notes on perspective of interview
+//primitive and non primitive
+//based on call by value and call by reference
+/*
+7 types
+string ,number ,boolean ,null ,symbol,BigInt,undefined
+
+
+reference (non primitive)
+// return type of non primitive data types is object and if it is 
+a function, it is object function
+
+arrays objects functions
+*/
+const id = Symbol('123')
+const newid = Symbol('123')
+console.log(id === newid)
+ console.log(typeof id);
+ 
+// array 
+const heroes = [1, 2, 3, 4, "ert"]
+
+// object
+
+let myobj = {
+    name: "karthik",
+    age:22
+}
+console.log(myobj.name);
+
+let myfun = function () {
+    console.log("hello world")
+}
+myfun()
+console.log(typeof myfun);
+
